506fc8bb53b8a95ba5c621a23493a21d2108a25c
minhnn0812_personal <minhnguyen120898@gmail.com>
2023-06-09 14:33:37 +0700

    connect db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const PORT = 3055
 
 const server = app.listen(PORT, (req, res) => {
-    console.log('WSV eComerce start with port ' + PORT);
+    console.log('WSV eCommerce start with port ' + PORT);
 });
 
 process.on('SIGINT', () => {
